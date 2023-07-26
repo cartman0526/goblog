21340633e46ec9a2756fbd70c1cd3a292f22cf09
tangwenfeng <tangwenfeng0301@outlook.com>
2023-07-26 10:38:51 +0800

    main函数实现http服务demo

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func main() {
+	http.HandleFunc("/", mainHandler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func mainHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World!")
 }
