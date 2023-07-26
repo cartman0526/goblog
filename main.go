8c147b9b4e5f0b2d29e82006411a0b121a6f73ea
tangwenfeng <tangwenfeng0301@outlook.com>
2023-07-26 13:13:45 +0800

    基于switch实现访问多页面访问

--- main.go
+++ main.go
@@ -11,5 +11,13 @@ func main() {
 }
 
 func mainHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World!")
+	switch r.URL.Path {
+	case "/":
+		fmt.Fprint(w, "<h1>这是主页</h1>")
+	case "/about":
+		fmt.Fprint(w, "<h1>这是一个网站简介</h1>"+
+			"<a href=\"mailto:tangwenfeng0301@outlook.com\">tangwenfeng0301@outlook.com</a>")
+	default:
+		fmt.Fprint(w, "<h1>请求页面未找到 :( </h1>")
+	}
 }
